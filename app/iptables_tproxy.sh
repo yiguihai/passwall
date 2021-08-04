9ab6f5edc59263fe210e639024a53299ad6dba55
yiguihai <18776473475@163.com>
2021-08-04 19:15:39 +0800

    Add files via upload

--- app/iptables_tproxy.sh
+++ app/iptables_tproxy.sh
@@ -31,19 +31,13 @@ wan_ifname=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
 
 acl() {
 	if [ ! -s /tmp/bypass-china.acl ]; then
-		cat >/tmp/bypass-china.tmp <<EOF
-$(curl -s https://proxy.freecdn.workers.dev/?url=https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt | grep -oE '([0-9]+\.){3}[0-9]+?\/[0-9]{1,2}')
-$(curl -s https://bgp.space/china.html | grep -oE '([0-9]+\.){3}[0-9]+?\/[0-9]{1,2}')
-$(curl -s https://bgp.space/china6.html | grep -oE '([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}\/[0-9]{1,3}')
-EOF
-		sort /tmp/bypass-china.tmp | uniq -u >/tmp/bypass-china2.tmp
 		cat >/tmp/bypass-china.acl <<EOF
 [proxy_all]
 
 [bypass_list]
-$(cat /tmp/bypass-china2.tmp)
+$(curl -s https://bgp.space/china.html | grep -oE '([0-9]+\.){3}[0-9]+?\/[0-9]{1,2}')
+$(curl -s https://bgp.space/china6.html | grep -oE '([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}\/[0-9]{1,3}')
 EOF
-		rm -f /tmp/bypass-china.tmp /tmp/bypass-china2.tmp
 	fi
 }
 up() {
@@ -62,27 +56,40 @@ down() {
 	if [ -s /tmp/ss.pid ]; then
 		read sspid </tmp/ss.pid
 		kill $sspid
-		rm -f /tmp/ss.pid
+		#rm -f /tmp/ss.pid
 	fi
 }
-#有acl还不够必须再加上一个中国路由表，因为流量进入sslocal分流后不会再经过nat表的PREROUTING链而是从OUTPUT链发出所以造成网易云解锁失败，
+
 add_rule() {
 	acl
 	up
 	ip -4 route add local 0/0 dev lo table 100
 	ip -4 rule add fwmark 0x2333/0x2333 table 100
+	#https://m.itbiancheng.com/linux/4961.html
 	ipset create sslan4 hash:net family inet -exist
+	ipset create chnip hash:net family inet -exist
 	iptables -w -t mangle -N SS
 	#iptables -w -t mangle -A SS -p udp -j LOG --log-prefix '** SUSPECT ** '
 	for i in ${lan_ipv4[@]}; do
 		ipset add sslan4 $i
 	done
+	if [ ! -s /tmp/chnip.ipset ]; then
+		#有acl还不够必须再加上一个中国路由表，因为流量进入sslocal分流后不会再经过nat表的PREROUTING链而是从OUTPUT链发出所以造成网易云解锁失败，(大概是这样)
+		echo "正在添加ipset规则..."
+		for i in $(curl -s https://proxy.freecdn.workers.dev/?url=https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt | grep -oE '([0-9]+\.){3}[0-9]+?\/[0-9]{1,2}'); do
+			ipset add chnip $i
+		done
+		ipset save chnip -f /tmp/chnip.ipset
+	else
+		ipset restore -f /tmp/chnip.ipset
+	fi
 	#https://unix.stackexchange.com/questions/383521/find-all-ethernet-interface-and-associate-ip-address
 	#for i in $(ip -o -4 addr show | awk -- '{print $4}'); do
 	#iptables -w -t mangle -A SS -d $i -j RETURN
 	#done
 	iptables -w -t mangle -A SS -i $wan_ifname -j RETURN
 	iptables -w -t mangle -A SS -m set --match-set sslan4 dst -j RETURN
+	iptables -w -t mangle -A SS -m set --match-set chnip dst -j RETURN
 	iptables -w -t mangle -A SS -p tcp -s 192.168.1.181 -j TPROXY --tproxy-mark 0x2333/0x2333 --on-ip 127.0.0.1 --on-port 60080
 	iptables -w -t mangle -A SS -p udp -s 192.168.1.181 -j TPROXY --tproxy-mark 0x2333/0x2333 --on-ip 127.0.0.1 --on-port 60080
 	iptables -w -t mangle -A PREROUTING -j SS
@@ -96,6 +103,7 @@ del_rule() {
 	iptables -w -t mangle -X SS
 	iptables -w -t nat -D PREROUTING -p udp -s 192.168.1.181 --dport 53 -j REDIRECT --to-ports 60053
 	ipset destroy sslan4
+	ipset destroy chnip
 	down
 }
 
